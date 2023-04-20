4edc64a49d9284c39716f6ae2e9811d1e3f50dcf
Jheferson <jhefersonvds@gmail.com>
2023-04-20 17:49:18 -0300

    first commit

---
+++ src/storage/group/groupCreate.ts
@@ -0,0 +1,8 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export async function groupCreate(newGroup: string) {
+  try {
+  } catch (error) {}
+
+  await AsyncStorage.setItem(`@group:${group.id}`, JSON.stringify(group));
+}
